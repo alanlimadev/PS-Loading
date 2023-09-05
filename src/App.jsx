0455b6d64a48fef93cc03527d567dc94933aae65
Alan Lima <alanreislm@gmail.com>
2023-09-05 20:03:15 -0300

    estruturando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 
 function App() {
-  
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ]);
 
-  return (
-    <div>Olá, React!</div>
-  )
+  return <div className="app">
+    <h1>TODO</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className="category">{todo.category}</p>
+          </div>
+          <div>
+            <button>✓</button>
+            <button>✕</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
 }
 
-export default App
+export default App;
