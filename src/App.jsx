ef8c9c213d43aa8c951a1f299cc21227cc2c98f6
Alan Lima <alanreislm@gmail.com>
2023-09-11 20:12:09 -0300

    correção de bug no armazenamento local

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,43 @@ import TodoForm from './components/TodoForm';
 import Search from './components/Search';
 import Filter from './components/Filter';
 
+
 function App() {
-  // Estado para as tarefas
   const [todos, setTodos] = useState([]);
 
-  // Estados para filtros e pesquisa
-  const [filter, setFilter] = useState("All");
-  const [category, setCategory] = useState("All");
-  const [order, setOrder] = useState("Asc");
   const [search, setSearch] = useState("");
 
-  // Função para adicionar uma tarefa
-  const addTarefa = (text, category) => {
-    const newTask = {
-      id: Math.floor(Math.random() * 10000),
-      text,
-      category,
-      isCompleted: false,
-    };
+  const [filter, setFilter] = useState("All");
 
-    // Atualiza a lista de tarefas
-    setTodos([...todos, newTask]);
-  };
+  const [category, setCategory] = useState("All");
+
+  const [order, setOrder] = useState("Asc");
+
+    // Função para adicionar uma tarefa
+    const addTarefa = (text, category) => {
+      // Cria uma nova tarefa
+      const newTask = {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      };
+  
+      // Atualiza a lista de tarefas no estado
+      const newTodos = [...todos, newTask];
+      setTodos(newTodos);
+  
+      // Armazena a lista atualizada no localStorage
+      localStorage.setItem("todos", JSON.stringify(newTodos));
+    };
 
   // Função para remover uma tarefa
   const removerTarefa = (id) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
+
+    // Atualiza o localStorage após a remoção
+    localStorage.setItem("todos", JSON.stringify(newTodos));
   };
 
   // Função para finalizar uma tarefa
@@ -43,6 +53,9 @@ function App() {
       return todo;
     });
     setTodos(newTodos);
+
+    // Atualiza o localStorage após a alteração do estado
+    localStorage.setItem("todos", JSON.stringify(newTodos));
   };
 
   // Efeito para carregar os dados do localStorage quando o componente é montado
@@ -52,27 +65,6 @@ function App() {
       setTodos(JSON.parse(storedTodos));
     }
   }, []);
-
-  // Filtragem e ordenação das tarefas
-  const filteredTodos = todos
-    .filter((todo) => {
-      if (filter === "All") return true;
-      if (filter === "Completed") return todo.isCompleted;
-      if (filter === "Uncompleted") return !todo.isCompleted;
-      return false;
-    })
-    .filter((todo) => {
-      if (category === "All") return true;
-      return todo.category === category;
-    })
-    .filter((todo) =>
-      todo.text.toLowerCase().includes(search.toLowerCase())
-    )
-    .sort((a, b) => {
-      if (order === "Asc") return a.text.localeCompare(b.text);
-      if (order === "Desc") return b.text.localeCompare(a.text);
-      return 0;
-    });
 
   return (
     <div className="app">
@@ -87,14 +79,47 @@ function App() {
           setCategory={setCategory}
           setOrder={setOrder}
         />
-        {filteredTodos.map((todo) => (
-          <Todo
-            key={todo.id}
-            todo={todo}
-            removerTarefa={removerTarefa}
-            finalizarTarefa={finalizarTarefa}
-          />
-        ))}
+        {todos
+          .filter((todo) => {
+            // Filtra com base no status
+            if (filter === "All") {
+              return true;
+            } else if (filter === "Completed") {
+              return todo.isCompleted;
+            } else if (filter === "Uncompleted") {
+              return !todo.isCompleted;
+            }
+            return false;
+          })
+          .filter((todo) => {
+            // Filtra com base na categoria
+            if (category === "All") {
+              return true;
+            }
+            return todo.category === category;
+          })
+          .sort((a, b) => {
+            // Ordena alfabeticamente
+            if (order === "Asc") {
+              return a.text.localeCompare(b.text);
+            } else if (order === "Desc") {
+              return b.text.localeCompare(a.text);
+            }
+            return 0;
+          })
+          .filter((todo) =>
+            // Filtra com base na pesquisa
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            // Mapeia cada tarefa para o componente Todo
+            <Todo
+              key={todo.id}
+              todo={todo}
+              removerTarefa={removerTarefa}
+              finalizarTarefa={finalizarTarefa}
+            />
+          ))}
       </div>
     </div>
   );
